80879900b1aeb50bc85bb48393bf132a68de8346
admiral-shlork <vvhtvr@protonmail.ch>
2025-02-15 07:43:34 +0000

    switched to legacy NVidia driver

--- blank-configuration/hardware-configuration.nix
+++ blank-configuration/hardware-configuration.nix
@@ -2,6 +2,7 @@
 
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.config.nvidia.acceptLicense = true;
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
@@ -13,7 +14,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
   };
 
   hardware.bluetooth.enable = true;
