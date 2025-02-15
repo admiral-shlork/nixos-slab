2ae5b2e4c1a6974d2ed2df8c2f0ecb47d9058c09
admiral-shlork <vvhtvr@protonmail.ch>
2025-02-15 14:23:46 +0000

    opened TCP port 9091 on fw

--- blank-configuration/networking-configuration.nix
+++ blank-configuration/networking-configuration.nix
@@ -4,6 +4,6 @@
   networking.hostName = "slab";
   networking.useDHCP = lib.mkDefault true;
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 8096 8920 ];
+  networking.firewall.allowedTCPPorts = [ 8096 8920 9091 ];
   networking.firewall.allowedUDPPorts = [ 1900 7359 ];
 }
