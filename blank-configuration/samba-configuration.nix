319f1b086a27fbfd427bdd84ea82351700440650
admiral-shlork <vvhtvr@protonmail.ch>
2025-02-17 20:24:01 +0000

    fixed syntax

--- blank-configuration/samba-configuration.nix
+++ blank-configuration/samba-configuration.nix
@@ -27,7 +27,7 @@ services.samba = {
       browseable = "yes";
       "read only" = "no";
       "valid users" = "whatever justyna";
-      "force user" = "whatever"
+      "force user" = "whatever";
     };
   };
   settings = {
