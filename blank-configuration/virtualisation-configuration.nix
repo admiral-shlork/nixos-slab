268a3c3058e3819b932ef11242df0d8b0e424b37
admiral-shlork <vvhtvr@protonmail.ch>
2025-02-26 18:15:09 +0000

    changed docker runtime config

--- blank-configuration/virtualisation-configuration.nix
+++ blank-configuration/virtualisation-configuration.nix
@@ -7,7 +7,7 @@
     };
     docker = {
       enable = true;
-      extraOptions = "--runtime=nvidia";
+      extraOptions = "--default-runtime=nvidia";
     };
   };
   users.groups.libvirtd.members = ["root" "whatever" ];
