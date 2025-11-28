9c99434a3aec9d89aa72c07cbd065db2e432e6ae
admiral-shlork <vvhtvr@protonmail.ch>
2025-11-28 14:34:47 +0000

    removing some packages

--- home.nix
+++ home.nix
@@ -13,39 +13,22 @@
     homeDirectory = "/home/whatever";
     stateVersion = "24.11";
     packages = with pkgs; [
-      audacity
-      calibre
       deadbeef
-      deluge-gtk
-      discord
       docker
       dropbox
       easytag
-      element-desktop
       evince
-      firefox-devedition
       floorp
-      gimp
       gnome-screenshot
-      jetbrains.pycharm-community
       keepassxc
       libreoffice
       librewolf
-      # lutris
-      # mangohud
-      megasync
-      obsidian
-      parsec-bin
       protonvpn-gui
       python3
       python311Packages.pip
-      signal-desktop
       soulseekqt
       soundconverter
       # steam
-      telegram-desktop
-      thunderbird
-      ungoogled-chromium
       winbox
       wine
       # virtualbox
