8bdd544cb0f077303063a8576e01c3fa4a76f900
admiral-shlork <vvhtvr@protonmail.ch>
2026-06-25 17:44:25 +0100

    changed the nvidia package

--- blank-configuration/hardware-configuration.nix
+++ blank-configuration/hardware-configuration.nix
@@ -10,7 +10,7 @@
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
     };
     nvidia-container-toolkit = {
       enable = true;
